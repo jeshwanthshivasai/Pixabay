0cbbf99342373300672c5db66aa4fe7e5e2b14aa
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-09-27 08:49:36 +0530

    my god!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@
 
 // const NavBar = () => {
 //     return (
-//         <nav>
-//             <div  className="logo"> <img src="https://s.alamy.com/logos/1.68.0/alamy.svg" alt="alamy" /> </div>
-//             <ul className="nav-links">
-//                 <div id="nav-right">
-//                     <input type="search" placeholder="Search here for Photos & Videos..." />
-//                     <span id="s1"> <button> Sign in </button>  </span>
-//                     <span id="s1"> <button> Sign up </button>  </span>
-//                 </div>
-//             </ul>
-//         </nav>
+        // <nav>
+        //     <div  className="logo"> <img src="https://s.alamy.com/logos/1.68.0/alamy.svg" alt="alamy" /> </div>
+        //     <ul className="nav-links">
+        //         <div id="nav-right">
+        //             <input type="search" placeholder="Search here for Photos & Videos..." />
+        //             <span id="s1"> <button> Sign in </button>  </span>
+        //             <span id="s1"> <button> Sign up </button>  </span>
+        //         </div>
+        //     </ul>
+        // </nav>
 //     )
 // }
 
--- src/components/Pixabay.jsx
+++ src/components/Pixabay.jsx
@@ -1,1 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+export default function Pixabay() {
+    let[api, setApi] = useState([]);
+    let[search, setSearch] = useState("");
+    let change = (e) => {
+        setSearch(e.target.value);
+    }
+    useEffect(() => {
+        let api = fetch(``);
+        api.then(res.json())
+        .then(val  => setApi(val.hits))
+    })
+
+    return (
+        <>
+        <nav>
+            <div  className="logo"> <img src="https://s.alamy.com/logos/1.68.0/alamy.svg" alt="alamy" /> </div>
+            <ul className="nav-links">
+                <div id="nav-right">
+                    <input type="search" placeholder="Search here for Photos & Videos..." />
+                    <span id="s1"> <button> Sign in </button>  </span>
+                    <span id="s1"> <button> Sign up </button>  </span>
+                </div>
+            </ul>
+        </nav>
+        <div className='header-image'>
+            <div>
+                <h1>Pixabay</h1>
+                <input type="text" name="" id="" placeholder='Search for anything...' onChange={change}/>
+            </div>
+        </div>
+        </>
+    )
+}
