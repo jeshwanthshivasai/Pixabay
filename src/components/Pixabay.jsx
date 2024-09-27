0ed5c7674d5201563cc8948153fd1f46067bf569
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-27 09:07:11 +0530

    probably done

--- src/components/Pixabay.jsx
+++ src/components/Pixabay.jsx
@@ -7,7 +7,7 @@ export default function Pixabay() {
         setSearch(e.target.value);
     }
     useEffect(() => {
-        let api = fetch(``);
+        let api = fetch(`https://pixabay.com/api/?key=46193687-1ee9f7465fd5eeca96a352d5f&q=${search}&image_type=photo`);
         api.then(res.json())
         .then(val  => setApi(val.hits))
     })
@@ -15,21 +15,54 @@ export default function Pixabay() {
     return (
         <>
         <nav>
-            <div  className="logo"> <img src="https://s.alamy.com/logos/1.68.0/alamy.svg" alt="alamy" /> </div>
-            <ul className="nav-links">
-                <div id="nav-right">
-                    <input type="search" placeholder="Search here for Photos & Videos..." />
-                    <span id="s1"> <button> Sign in </button>  </span>
-                    <span id="s1"> <button> Sign up </button>  </span>
-                </div>
-            </ul>
-        </nav>
-        <div className='header-image'>
-            <div>
-                <h1>Pixabay</h1>
-                <input type="text" name="" id="" placeholder='Search for anything...' onChange={change}/>
+                <h1>pixabay</h1>
+                <ul>
+                    <div id="nav-p">
+                        <li id="nav-items"><button>Explore</button></li>
+                        <li id="nav-items"><button>Login</button></li>
+                        <li id="nav-items"><button>Join</button></li>
+                        <li id="nav-items"><button>Upload</button></li>
+                    </div>
+                </ul>
+            </nav>
+            <div className="header-image"></div>
+            <div className="main-container">
+                <h1 id="main-head-t">Stunning royalty-free images & royalty-free stock</h1>
+
+
+                <h3>Over 5 million+ high-quality stock images, videos, and music shared by our talented community.</h3>
+
+                <input type="text" placeholder="Search for all images on Pixabay" onChange={change} />
+                
+                <select name="" id="select">
+                    <option value="">All Images</option>
+                    <option value="">Photos</option>
+                    <option value="">Vectors</option>
+                </select>
             </div>
-        </div>
+
+            <div className="button-container">
+                <button className="styled-button">Home</button>
+                <button className="styled-button">Photos</button>
+                <button className="styled-button">Gifs</button>
+                <button className="styled-button">Illustrations</button>
+            </div> <br />   <br />
+
+            <div>
+                <div id="last-container">
+                    {api.filter(x => {
+                        return (
+                            x.tags.toLowerCase().includes(search)
+                        )
+                    })
+                        .map(x => {
+                            return (
+                                <img src={x.webformatURL} id="lastImgae" />
+                            )
+                        })}
+
+                </div>
+            </div>
         </>
     )
 }
